f99ede32cbd897bf3e2589f53d28c5a19c47291d
pixelynx <tjstevens1992@gmail.com>
2025-06-28 00:42:34 -0400

    Revert "[deploy] test compatibility date"
    This reverts commit 5d13648bc711e2df535b2c9ee0eeca9734e823d6.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 export default defineNuxtConfig({
   // Development and compatibility settings
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
   
   // Modules
